0af4deacca3f9c4c04754e53b7512db84bcbc79f
Edward Phillips <edphil@hotmail.co.uk>
2019-12-10 16:49:55 +0100

    inserted plane into cube's space

--- orbit.js
+++ orbit.js
@@ -1,13 +1,16 @@
 const cube = document.querySelector('.cube');
+const plane = document.querySelector('.plane');
 
 let rotationX = 0;
 let rotationY = 0;
 
 const rotateCube = () => {
   rotationX += 1;
-  rotationY += 2;
+  rotationY += 1;
   cube.style.transform = `rotateY(${rotationX}deg)`;
-  cube.style.transform += `rotateX(${rotationY}deg)`;
+  // cube.style.transform += `rotateX(${rotationY}deg)`;
+  plane.style.transform = `rotateY(${-rotationX}deg)`;
+  // plane.style.transform = `rotateX(${-rotationY}deg)`;
   window.requestAnimationFrame(rotateCube);
 }
 
