4671aa6643e0a9bc11f7b467c92ca7fb6bdff102
Edward Phillips <edphil@hotmail.co.uk>
2019-12-11 09:06:39 +0100

    orbit done

--- orbit.js
+++ orbit.js
@@ -1,16 +1,18 @@
 const cube = document.querySelector('.cube');
-const plane = document.querySelector('.plane');
+const orbitCube = document.querySelector('.orbit-cube');
+const orbit = document.querySelector('.orbit');
 
 let rotationX = 0;
 let rotationY = 0;
 
 const rotateCube = () => {
   rotationX += 1;
-  rotationY += 1;
+  rotationY += 2;
   cube.style.transform = `rotateY(${rotationX}deg)`;
-  // cube.style.transform += `rotateX(${rotationY}deg)`;
-  plane.style.transform = `rotateY(${-rotationX}deg)`;
-  // plane.style.transform = `rotateX(${-rotationY}deg)`;
+  cube.style.transform += `rotateX(${rotationY}deg)`;
+  orbitCube.style.transform = `rotateY(${-rotationX}deg)`;
+  orbitCube.style.transform += `rotateX(${-rotationY}deg)`;
+  orbit.style.transform = `rotateY(${rotationY}deg)`;
   window.requestAnimationFrame(rotateCube);
 }
 
